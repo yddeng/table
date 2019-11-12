548a6b3fa26d38a3976a4b96ea52d8e80d47c4e8
tagDong <248244142@qq.com>
2019-11-12 22:19:09 +0800

    none

--- app/handsontable.js
+++ app/handsontable.js
@@ -15,7 +15,7 @@ handstable.new = function(){
         data: handstable.data,
 
         width:'1200px',
-        height:'800px',
+        height:'500px',
 
         minCols:46, //最小列数
         //maxCols: 50, //最大列数
@@ -27,17 +27,18 @@ handstable.new = function(){
         colHeaders:true, //显示列表头， 默认false， 可取 true/fals/数组 ，当值为数组时，列头为数组的值
         rowHeaders:true,　//显示行表头， 默认 false， 可取 true/fals/数组，当值为数组时，行头为数组的值
 
-        mergeCells: true, //表示允许单元格合并
+        //mergeCells: true, //表示允许单元格合并
 
         fixedRowsTop : 0,    //冻结行（固定顶部开始算起指定行不随垂直滚动条滚动；
         fixedColumnsLeft : 0, //冻结列（固定右边开始算起指定行不随水平滚动条滚动）；
+
         manualColumnFreeze:true,  //设置true后在单元格中右击出现一个菜单，此菜单会多出一个“Unfteeze this columu”的选项，再次点击就会取消冻结动作。 默认为false
 
         manualColumnResize: true, //允许拖曳列表头，默认为false
         manualRowResize: true, //允许拖曳行表头，默认为false
 
-        currentRowClassName:"cur", //给当前行设置样式名（鼠标点击某个单元格，则整行给样式）
-        currentColClassName:"cur", //给当前行列设置样式名（鼠标点击某个单元格，则整行给样式）
+        currentRowClassName:"curRow", //给当前行设置样式名（鼠标点击某个单元格，则整行给样式）
+        //currentColClassName:"cur", //给当前行列设置样式名（鼠标点击某个单元格，则整行给样式）
 
         autoColumnSize: true, //当值为true且列宽未设置时，自适应列大小
         //columnWidth:50,
@@ -51,39 +52,40 @@ handstable.new = function(){
         manualColumnMove: true, //true/false 当值为true时，列可拖拽移动到指定列
         manualRowMove: true, //true/false当值为true时，行可拖拽至指定行
 
-        manualColumnFreeze:true,
-
-        undo: true,
-        redo: true,
+        //undo: true,
+        //redo: true,
 
         contextMenu:true, //显示表头的下拉菜单默认false 可取 true/false/自定义数组
         contextMenu: {    //内容部分的menu 对功能汉化
             items: {
-                'row_above': {
-                    name: '上方插入一行'
-                },
-                'row_below': {
-                    name: '下方插入一行'
-                },
-                "col_left": {
-                    name: '左方插入列',
-                },
-                "col_right": {
-                    name: '右方插入列'
-                },
-                "remove_row": {
-                    name: '删除行',
-                },
-                "remove_col": {
-                    name: '删除列',
-                },
-                'separator': Handsontable.plugins.ContextMenu.SEPARATOR,
-                'clear_custom': {
-                    name: '清除数据',
-                    callback: function() {
-                        this.clear();
-                    }
+              'row_above': {
+                name: '上面插入一行'
+              },
+              'row_below': {
+                name: '下面插入一行'
+              },
+              'col_left':{
+                name: '左侧插入一列'
+              },
+              'col_right':{
+                name: '右侧插入一列'
+              },
+              'remove_row':{
+                name: '移除本行'
+              },
+              'remove_col':{
+                name: '移除本列'
+              },
+              'make_read_only':{
+                name:'只读'
+              },
+              'separator': Handsontable.plugins.ContextMenu.SEPARATOR,
+              'clear_custom': {
+                name: '清空所有单元格数据',
+                callback: function () {
+                  this.clear()
                 }
+              }
             }
         },
         afterChange:function(changes,source){
@@ -107,8 +109,6 @@ handstable.new = function(){
         },
         afterSelectionEnd: function(row, col, row2, col2) {
             console.log("afterSelectionEnd",row,col,row2,col2)
-            /*console.log($(".rowHeader").parent().parent().eq(row))
-            $(".rowHeader").parent().parent().eq(row).addClass('ht__highlight cur ht__active_highlight')*/
             var selections = new Array()
             for (var i = row;i <= row2;i++){
                 for (var j = col;j <= col2;j++){
